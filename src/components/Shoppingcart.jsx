06fc43c09f876f2aa92de0f3f00be14e618b35d5
Carolin Axelsson <carolin.91@outlook.com>
2022-03-28 12:30:31 +0200

    la till front end style för payment och länka från startsida

--- src/components/Shoppingcart.jsx
+++ src/components/Shoppingcart.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StripeContainer from './StripeContainer'
 
 const Shoppingcart = () => {
+
+  const [showItem, setShowItem] = useState(false);
+
   return (
     <div>
         <h1> Shopping Cart</h1>
+        <div>
+          <p>
+            {showItem ? <StripeContainer/> : <> <h3>Price: ex.10kr</h3> <img alt="exempelbild"/>
+            <button onClick={() => setShowItem(true)} >Fortsätt till secure payment </button> </>}
+          </p>
+        </div>
     </div>
   )
 }
