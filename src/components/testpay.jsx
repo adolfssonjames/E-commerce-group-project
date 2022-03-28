a998aa6b5cca17cd517d22459700f239a09e49fa
Carolin Axelsson <carolin.91@outlook.com>
2022-03-28 13:44:27 +0200

    test commit

---
+++ src/components/testpay.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import '../CSS/Payment.css';
+import StripeContainer from './StripeContainer'
+
+const Shoppingcart = () => {
+
+  const [showItem, setShowItem] = useState(false);
+
+  return (
+    <div>
+        <h1> Shopping Cart</h1>
+        <div>
+          <p>
+            {showItem ? <StripeContainer/> : <> <h3>Price: ex.10kr</h3> <img alt="exempelbild"/>
+            <button onClick={() => setShowItem(true)} >Fortsätt till secure payment </button> </>}
+          </p>
+        </div>
+    </div>
+  )
+}
+
+export default Shoppingcart
