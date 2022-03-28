47bf307941c0e8fde6f8a4cd0644f14fc7036d67
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-03-28 17:41:20 +0200

    cart updated

--- src/components/Shoppingcart.jsx
+++ src/components/Shoppingcart.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../CSS/Payment.css';
+import StripeContainer from './StripeContainer'
+import { useCart } from 'react-use-cart';
 
 const Shoppingcart = () => {
+
+  const [showItem, setShowItem] = useState(false);
+
+  const {
+    isEmpty,
+    items,
+    totalItems, 
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart
+  } = useCart()
+
+  if (isEmpty) return <h2>Din varukorg är tom...</h2> 
+
   return (
     <div>
         <h1> Shopping Cart</h1>
+        <div>
+          <p>
+            {showItem ? <StripeContainer/> : <> <h3>Price: ex.10kr</h3> <img alt="exempelbild"/>
+            <button onClick={() => setShowItem(true)} >Fortsätt till secure payment </button> </>}
+          </p>
+        </div>
     </div>
   )
 }
