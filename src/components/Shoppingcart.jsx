fa8c835818c544502784b3339fe7f82bfadce6e7
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-03-28 12:58:33 +0200

    updates

--- src/components/Shoppingcart.jsx
+++ src/components/Shoppingcart.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useCart } from "react-use-cart"
 
 const Shoppingcart = () => {
+
+  const {
+    isEmpty,
+    items,
+    totalItems,
+    cartTotal,
+    removeItem,
+    emptyCart
+  } = useCart();
+
+  if (isEmpty) return <h2>Your cart is empty...</h2>
+
   return (
     <div>
-        <h1> Shopping Cart</h1>
+        <h1>Shopping Cart</h1>
     </div>
   )
 }
