fd042859647d198479d7263fa65b010416bff3f5
fred <borgfred@hotmail.se>
2022-03-29 15:43:38 +0200

    La till produktsida

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import "../CSS/Products.css";
 
 const Products = () => {
   return (
     <div>
-        <h1> Products </h1>
+      <h1> Products </h1>
+      <div id="articles">
+        <div class="product">
+          <img
+            src="../img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Dinicus Mamicus"
+          />
+          <h2>Dinicus Mamicus</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Dinicus Mamicus"
+          />
+          <h2>Dinicus Mamicus</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Dinicus Mamicus"
+          />
+          <h2>Dinicus Mamicus</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Dinicus Mamicus"
+          />
+          <h2>Dinicus Mamicus</h2>
+          <h3>69;-</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
