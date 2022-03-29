39bf06bd926e6621a4a60011644290266a70c4cb
fred <borgfred@hotmail.se>
2022-03-29 15:55:47 +0200

    changes to product page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,33 +9,65 @@ const Products = () => {
         <div class="product">
           <img
             src="../img/pexels-quang-nguyen-vinh-2149105.jpg"
-            alt="Dinicus Mamicus"
+            alt="Quercus robur"
           />
-          <h2>Dinicus Mamicus</h2>
+          <h2>Quercus robur</h2>
           <h3>69;-</h3>
         </div>
         <div class="product">
           <img
             src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
-            alt="Dinicus Mamicus"
+            alt="P. sylvestris"
           />
-          <h2>Dinicus Mamicus</h2>
+          <h2>P. sylvestris</h2>
           <h3>69;-</h3>
         </div>
         <div class="product">
           <img
             src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
-            alt="Dinicus Mamicus"
+            alt="Betula"
           />
-          <h2>Dinicus Mamicus</h2>
+          <h2>Betula</h2>
           <h3>69;-</h3>
         </div>
         <div class="product">
           <img
             src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
-            alt="Dinicus Mamicus"
+            alt="Picea abies"
           />
-          <h2>Dinicus Mamicus</h2>
+          <h2>Picea abies</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Populus tremula"
+          />
+          <h2>Populus tremula</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Acer platanoides"
+          />
+          <h2>Acer platanoides</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Salix"
+          />
+          <h2>Salix</h2>
+          <h3>69;-</h3>
+        </div>
+        <div class="product">
+          <img
+            src="../src/img/pexels-quang-nguyen-vinh-2149105.jpg"
+            alt="Sorbus aucuparia"
+          />
+          <h2>Sorbus aucuparia</h2>
           <h3>69;-</h3>
         </div>
       </div>
