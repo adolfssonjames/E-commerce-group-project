1445954f39dfe3ed4d2d6c1e8eb9786f8b5af075
Robert Lockett <lockettr@hotmail.com>
2022-03-30 16:27:44 +0200

    jobbade lite med myAccount bootstrap

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { useRef } from 'react';
 import '../CSS/MyAccount.css';
+import {Card, Form, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyAccount() {
   
-
+    const emailRef = useRef()
+    const passwordRef = useRef()
   
   
   
     return (
-    <div>
+    <>
+        <Card>
+            <Card.Body>
+                <h2 classname='text-center mb-4'>My Account</h2>
+                <Form>
+                    <Form.Group id='currentEmail'>
+                        <Form.Label>Current Email</Form.Label>
+                        <Form.Control type='email' ref={emailRef} required></Form.Control>
+                    </Form.Group>
+                    <Form.Group id='currentPassword'>
+                        <Form.Label>Current Password</Form.Label>
+                        <Form.Control type='password' ref={passwordRef} required></Form.Control>
+                    </Form.Group>
+                    <Button className='w-50' type='submit'>Submit</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        
+    </>
+  )
+}
+
+
+/*<div>
         <form className='test'>
             <label htmlFor="updateEmailInputField">email:</label>
             <input type="text" id="updateEmailInputField"/>
@@ -23,8 +50,6 @@ function MyAccount() {
                 <th>Orders</th>
             </tr>
         </table>
-    </div>
-  )
-}
+    </div>*/
 
 export default MyAccount
