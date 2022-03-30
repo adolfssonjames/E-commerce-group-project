d4e62a5019f96150812d4da0f2f012d4e365f829
James <adolfssonjames@gmail.com>
2022-03-30 02:05:05 +0200

    firebase-login etc

---
+++ src/components/Profile.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useAuth, upload } from "../firebase/firebase";
+import '../CSS/Mypage.css';
+
+
+export default function Profile()  {
+    const currentUser = useAuth();
+    const [photo, setPhoto] = useState (null);
+    const [loading, setLoading] = useState (false);
+    const [photoURL, setPhotoURL] = useState("https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=170667a&w=0&h=kEAA35Eaz8k8A3qAGkuY8OZxpfvn9653gDjQwDHZGPE=");
+
+    function handleChange(e) {
+       if (e.target.files[0]) {
+           setPhoto(e.target.files[0])
+       }
+    }
+
+    function handleClick () {
+        upload(photo, currentUser, setLoading);
+    }
+
+    useEffect(() => {
+        if (currentUser && currentUser.photoURL) {       
+            setPhotoURL(currentUser.photoURL);
+         }
+
+    },[currentUser])
+
+    
+
+    return(
+        <div className="fields">
+            <input type="file" onChange={handleChange} />
+            <button disabled={loading || !photo} onClick={handleClick}>Upload avatar</button>
+            <img src={photoURL}
+                 alt="avatar"
+                 className="user-avatar"/>
+        </div>
+
+
+    );
+}
