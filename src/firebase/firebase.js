b9ee44b3f62685eacd01f6fdbe2e6b03e6898274
James <adolfssonjames@gmail.com>
2022-03-30 15:37:43 +0200

    some css

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -22,7 +22,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(); 
-const storage = getStorage();
+const storage = getStorage(app);
+
 
 export function signup(email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -64,5 +65,5 @@ export async function upload(file, currentUser, setLoading) {
     updateProfile(currentUser, {photoURL});
     
     setLoading(false);
-    alert("Uploaded file!");
+    alert("file has been uploaded");
   }
