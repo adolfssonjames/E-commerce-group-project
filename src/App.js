7dc85a2d14174dc986f3727fa9ddeeaa7b2d18b4
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-01 20:03:29 +0200

    Added shopping cart icon

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import Shoppingcart from './components/Shoppingcart';
 import About from './components/About';
 import StripeContainer from './components/StripeContainer';
 import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-import { CartProvider, useCart } from "react-use-cart"
+import { CartProvider } from "react-use-cart"
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart"
 
 function App() {
-
-const {
-  totalItems
-} = useCart()
 
   return (
 
@@ -27,11 +24,10 @@ const {
                  <li> <Link to="/" className="link">Home</Link></li>
                   <li> <Link to="/Products" className="link">Products</Link></li>
                   <li> <Link to="/About" className="link">About</Link></li> 
-              </ul>
-                  
+              </ul>    
               <ul className='navbar-right'>
                    <li> <Link to="/Mypage" className="link">My page</Link></li>
-                   <li> <Link to="/Shoppingcart" className="link">Shoppingcart ({totalItems})</Link></li>  
+                   <li> <Link to="/Shoppingcart" className="link"><ShoppingCartIcon /></Link></li>  
               </ul>
           </div>    
 
