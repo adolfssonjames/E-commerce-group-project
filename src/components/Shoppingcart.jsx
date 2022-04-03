38f6c32b83b078359f3359a079dd3486b53d4b13
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-03 19:06:41 +0200

    adds design to shoppingcart

--- src/components/Shoppingcart.jsx
+++ src/components/Shoppingcart.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useCart } from 'react-use-cart';
 import { Link } from 'react-router-dom';
+import '../CSS/Cart.css';
 
 const Shoppingcart = () => {
 
@@ -20,45 +21,46 @@ const Shoppingcart = () => {
 
 
 
-  return (
+ return (
   <section>
 
-    <div>
-    <h1>Shopping cart ({totalItems})</h1>
+  <h1 className='CartHead'>Shopping cart ({totalItems})</h1>
 
-      <table className='CartTable'>
-      
-      <tbody className='CartTbody'>
+    <div className='ShoppingCart'>
 
       {items.map((cartitem) => {
         return (
 
+        <table>
+        <tbody>
+
         <tr key={cartitem.id}>
 
-          <td><img src={cartitem.image} style={{height: '6rem'}}></img></td>
-          <td><h5>{cartitem.name}</h5></td>
-          <td><h5>${cartitem.price}</h5></td>
-          <td><h5>x{cartitem.quantity}</h5></td>
+          <td><img className='ItemImg' src={cartitem.image}></img></td>
+          <td><h5 className='ItemName'>{cartitem.name}</h5></td>
+          <td><h5 className='ItemPrice'>${cartitem.price}</h5></td>
+          <td><h5 className='ItemQty'>{cartitem.quantity}</h5></td>
           <td><button className='MinusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity -1)}>-</button></td>
           <td><button className='PlusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity +1)}>+</button></td>
           <td><button className='RemoveBtn' onClick={() => removeItem(cartitem.id)}>Remove items</button></td>
         
         </tr>
 
+        </tbody>
+        </table>
+
           )
       })}
 
-      </tbody>
-      </table>
     </div>
     
     <div>
-      <h4>Total Price: ${cartTotal}</h4>
+      <h4 className='TotalPrice'>Total Price: ${cartTotal}</h4>
      </div>
 
-     <div>
-     <button><Link to="/payment"> Payment </Link></button>
-     </div>
+       <button className='PayBtn'><Link to="/payment" className="PayLink"> Payment </Link></button>
+       <button className='EmptyBtn' onClick={emptyCart}> Empty cart </button>
+
   </section>
 
 
