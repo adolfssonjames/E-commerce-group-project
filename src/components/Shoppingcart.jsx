29330d8aa7fc038cc83f19273ee48c9991d1ee17
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-03 22:48:49 +0200

    css updates in cart

--- src/components/Shoppingcart.jsx
+++ src/components/Shoppingcart.jsx
@@ -17,8 +17,7 @@ const Shoppingcart = () => {
 
  } = useCart()
  
- if (isEmpty) return <h2>Your cart is empty...</h2>
-
+ if (isEmpty) return <h1 className="CartEmpty">Your cart is empty...</h1>
 
 
  return (
@@ -26,40 +25,39 @@ const Shoppingcart = () => {
 
   <h1 className='CartHead'>Shopping cart ({totalItems})</h1>
 
-    <div className='ShoppingCart'>
+  <div className='ShoppingCart'>
 
-      {items.map((cartitem) => {
-        return (
-
-        <table>
+  <table>
         <tbody>
+            {items.map((cartitem) => {
+              return (
 
-        <tr key={cartitem.id}>
+              <tr key={cartitem.id}>
 
-          <td><img className='ItemImg' src={cartitem.image}></img></td>
-          <td><h5 className='ItemName'>{cartitem.name}</h5></td>
-          <td><h5 className='ItemPrice'>${cartitem.price}</h5></td>
-          <td><h5 className='ItemQty'>{cartitem.quantity}</h5></td>
-          <td><button className='MinusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity -1)}>-</button></td>
-          <td><button className='PlusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity +1)}>+</button></td>
-          <td><button className='RemoveBtn' onClick={() => removeItem(cartitem.id)}>Remove items</button></td>
-        
-        </tr>
+                <td><img className='ItemImg' src={cartitem.image}></img></td>
+                <td><h5 className='ItemName'>{cartitem.name}</h5></td>
+                <td><h5 className='ItemPrice'>${cartitem.price}</h5></td>
+                <td><h5 className='ItemQty'>{cartitem.quantity}</h5></td>
+                <td><button className='MinusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity -1)}>-</button></td>
+                <td><button className='PlusBtn' onClick={() => updateItemQuantity(cartitem.id, cartitem.quantity +1)}>+</button></td>
+                <td><button className='RemoveBtn' onClick={() => removeItem(cartitem.id)}>Remove items</button></td>
+              
+              </tr>
+
+                )
+            })}
 
         </tbody>
-        </table>
+  </table>
 
-          )
-      })}
-
-    </div>
+  </div>
     
-    <div>
+  <div>
       <h4 className='TotalPrice'>Total Price: ${cartTotal}</h4>
-     </div>
+  </div>
 
-       <button className='PayBtn'><Link to="/payment" className="PayLink"> Payment </Link></button>
-       <button className='EmptyBtn' onClick={emptyCart}> Empty cart </button>
+  <Link to="/payment" className="PayLink"><button className='PayBtn'> Payment </button></Link>
+  <button className='EmptyBtn' onClick={emptyCart}> Empty cart </button>
 
   </section>
 
