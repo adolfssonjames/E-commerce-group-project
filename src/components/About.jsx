7981b4bae8a3eca49e33520dd5f449f0b8b17a41
James <adolfssonjames@gmail.com>
2022-04-06 21:00:24 +0200

    lite about lite home css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,20 +20,48 @@ const About = () => {
                 Ad aut autem dolore. Ipsa eum, vel asperiores saepe rerum consectetur, reiciendis 
                 sed autem, pariatur sit molestias obcaecati porro voluptate provident nulla.
                 </p>
-                <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Eius laudantium fuga quis saepe quaerat aliquam atque, nesciunt ex, 
-                fugit doloremque cumque deserunt ducimus blanditiis hic autem debitis 
-                eligendi ipsa. Quaerat? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Ad aut autem dolore. Ipsa eum, vel asperiores saepe rerum consectetur, reiciendis 
-                sed autem, pariatur sit molestias obcaecati porro voluptate provident nulla.
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Eius laudantium fuga quis saepe quaerat aliquam atque, nesciunt ex, 
-                fugit doloremque cumque deserunt ducimus blanditiis hic autem debitis 
-                eligendi ipsa. Quaerat? Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Ad aut autem dolore. Ipsa eum, vel asperiores saepe rerum consectetur, reiciendis 
-                sed autem, pariatur sit molestias obcaecati porro voluptate provident nulla.
-              </p>
+                <div className='team' >
+                    <section className='member1'>
+                      <h4 className='name'> Romina</h4>
+                      <p className='description'> Founder of bonnie bonsai Webbdevs, Romina is a professional frontend developer who started her career with a Frontend developer education at Jensen Education</p>
+                      <p className='email' >Email:</p>
+                      <a href="mailto:Romina@bonniebonsai.com">Romina@bonniebonsai.com</a>
+                      <p className='phone'>Phone:</p>
+                      <a href="tel:+46 73 737 73 73">+46 73 737 73 73</a>
+                    </section>
+                    <section className='member'>
+                      <h4 className='name'> Robert</h4>
+                      <p className='description'> Founder of Bonnie Bonsai Webbdevs, Robert is a professional frontend developer who started his career with a Frontend developer education at Jensen Education</p>
+                      <p className='email' >Email:</p>
+                      <a href="mailto:Robert@bonniebonsai.com">Robert@bonniebonsai.com</a>
+                      <p className='phone'>Phone:</p>
+                      <a href="tel:+46 73 737 73 73">+46 73 737 73 73</a>
+                    </section>
+                    <section className='member3'>
+                      <h4 className='name'> Carolina</h4>
+                      <p className='description'> Founder of Bonnie Bonsai Webbdevs, Carolina is a professional frontend developer who started her career with a Frontend developer education at Jensen Education</p>
+                      <p className='email' >Email:</p>
+                      <a href="mailto:Carolina@bonniebonsai.com">Carolina@bonniebonsai.com</a>
+                      <p className='phone'>Phone:</p>
+                      <a href="tel:+46 73 737 73 73">+46 73 737 73 73</a>
+                    </section>
+                    <section className='member'>
+                      <h4 className='name'> James</h4>
+                      <p className='description'> Founder of Bonnie Bonsai Webbdevs, James is a professional frontend developer who started his career with a Frontend developer education at Jensen Education</p>
+                      <p className='email' >Email:</p>
+                      <a href="mailto:James@bonniebonsai.com">James@bonniebonsai.com</a>
+                      <p className='phone'>Phone:</p>
+                      <a href="tel:+46 73 737 73 73">+46 73 737 73 73</a>
+                    </section>
+                    <section className='member'>
+                      <h4 className='name'> Fred</h4>
+                      <p className='description'> Founder of Bonnie Bonsai Webbdevs, Fred is a professional frontend developer who started his career with a Frontend developer education at Jensen Education</p>
+                      <p className='email' >Email:</p>
+                      <a href="mailto:Fred@bonniebonsai.com">Fred@bonniebonsai.com</a>
+                      <p className='phone'>Phone:</p>
+                      <a href="tel:+46 73 737 73 73">+46 73 737 73 73</a>
+                    </section>
+                </div>
           </div>
   </section>
   )
