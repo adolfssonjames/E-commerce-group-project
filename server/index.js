89849426b3a768571c4d581cbb657c7ace3d31b5
Robert Lockett <lockettr@hotmail.com>
2022-04-06 21:14:49 +0200

    pushar innan fråga om hjälp

--- server/index.js
+++ server/index.js
@@ -48,5 +48,5 @@ app.use('/productImages', express.static('productImages'))
 
 
 app.listen(process.env.PORT || 4000, () => {
-	console.log("Sever is listening on port 4000")
+	console.log("Server is listening on port 4000")
 })
