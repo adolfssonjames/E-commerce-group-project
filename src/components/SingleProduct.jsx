682374a00f6e28a687f136b58d80fb3e936ae229
fred <borgfred@hotmail.se>
2022-04-08 22:42:24 +0200

    hoppsan något gick fel

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -9,14 +9,22 @@ const SingleProduct = () => {
   return (
     <div>
       <h2>Product</h2>
+      <main id="SingleProductMain">
+        <h3 id="SingleProductName">Hoppsan något gick fel</h3>
+        <p id="SingleProductDesc">Vi kunde inte hitta den här produkten</p>
 
-
+        <img
+          src="https://i.redd.it/7m9gyr3zw6s81.jpg"
+          alt=""
+          id="SingleProductImg"
+        />
         <button
           id="SingleProduct-to-cart-btn"
           //onClick={() => addItem(product)}
         >
           Add to cart
         </button>
+      </main>
     </div>
   );
 };
