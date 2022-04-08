37d17d8b49d4c8d4cd3e1e4d6a01378321863428
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-08 21:37:37 +0200

    Uppdateringar på Enskild produkt sidan

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -8,72 +8,15 @@ const SingleProduct = () => {
 
   return (
     <div>
-      <h2>Fungera för i helvete</h2>
-      <main id="SingleProductMain">
-        <h3 id="SingleProductName">Satans Avföring</h3>
-        <p id="SingleProductDesc">
-          Oändlig cum. Du sitter på toaletten för att jacka av, men du börjar
-          cum okontrollerat. Efter tio spurter börjar du bli orolig. Din hand är
-          klibbig och det luktar sperma. Du trycker desperat in din kuk i en
-          bunt toalettpapper, men det gör bara ont i bollarna. Sperman
-          accelererar. Det har gått tre minuter. Du kan inte sluta cumming. Ditt
-          badrumsgolv är täckt av ett tunt lager babyvätska. Du försöker komma
-          ner i duschavloppet men det byggs upp för snabbt. Du provar toaletten.
-          Sperman är för tjock för att spolas. Du låser badrumsdörren för att
-          förhindra att sperma kommer ut. Luften blir varm och fuktig från
-          sperma. Sperman accelererar. Du halkar och faller i din egen sperma.
-          Sperman är nu sex tum djup, nästan lika lång som din fortfarande
-          upprättstående spermaslang. Utspridda på ryggen börjar du spruta över
-          hela taket. Kulor av den klibbiga vita vätskan börjar falla som
-          regndroppar, vilket ger dig en ansiktsbehandling med din egen sperma.
-          Sperman accelererar. Du kämpar för att stå när kraften från sperma
-          börjar driva dig bakåt som om du var på en bukkake-tema
-          glid-och-slide. Fortfarande på knäna, sperma är nu i hakhöjd. För att
-          undvika att drunkna öppnar du badrumsdörren. Syndfloden av
-          människosaft påminner dig om den stora melassfloden 1919, bara med
-          sperma istället för melass. Sperman accelererar. Det har gått två
-          timmar. Era barn och fru skriker av skräck när deras kroppar uppslukas
-          av det snövita slammet. Ditt yngsta barn går under, med trögflytande
-          bubblor och dova gråt som stiger upp från goop. Du vädjar till Gud att
-          få ett slut på ditt lidande. Sperman accelererar. Du klämmer på din
-          kuk för att stoppa sperma, men det börjar läcka ut ur ditt rövhål
-          istället. Du släpper taget. Kraften från sperma river upp urinröret
-          och lämnar bara ett gapande hål i grenen som spyr ut sperma. Din kropp
-          tar fart när den glider bakåt längs med sperma. Du slår igenom väggen
-          och susar upp i himlen i trettio miles i timmen. Från ett
-          fågelperspektiv ser du att ditt hus är helt vitt. Din granne ringer
-          polisen. Sperman accelererar. När du fortsätter att stiga ser du
-          polisbilar som kör mot ditt hus. Polisen drar fram sina vapen och
-          siktar, men massor av sperma träffade dem i ögonen och förblindade
-          dem. Sperman accelererar. Du är nu på en höjd av 1000 fot. SWAT-teamet
-          anländer. Militära helikoptrar kretsar runt dig. Hundratals kulor
-          tränger igenom din kropp på en gång, men du förblir vid medvetande.
-          Dina testiklar har nu vuxit till en ersättningshjärna. Sperman
-          accelererar. Det har gått två dagar. Med din kropp nu förstörd börjar
-          sperma spraya åt alla håll. Du bryter ljudmuren. Regeringen sätter in
-          stridsflygplan för att jaga ner dig, men effekten av din sperma får
-          ett plan att krascha till marken. Regeringen beslutar att låta dig
-          lämna jorden. Du känner att dina könskörtlar börjar brinna upp när du
-          når kanterna av atmosfären. Du missar knappt ISS, vilket ger den en ny
-          vitmålning när du flyger förbi. Fysiker kämpar för att beräkna din
-          oberäkneliga bana. Sperman accelererar. Sperman börjar dras mot sig
-          själv och bildar ett kometspår av sperma. Astronomer börjar kalla dig
-          "Cummet". Du är fast i rymden för alltid, fråntagen din kropp och dina
-          sinnen, tvingad att utstå en evighet av sprutor. Till slut slutar du
-          tänka...
-        </p>
-        <img
-          src="https://i.redd.it/7m9gyr3zw6s81.jpg"
-          alt=""
-          id="SingleProductImg"
-        />
+      <h2>Product</h2>
+
+
         <button
           id="SingleProduct-to-cart-btn"
           //onClick={() => addItem(product)}
         >
           Add to cart
         </button>
-      </main>
     </div>
   );
 };
